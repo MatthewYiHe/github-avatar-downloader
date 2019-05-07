35abd02dcb246cad57f0a07df68742d79a35ba9e
MatthewYiHe <matthewhe1@hotmail.com>
2019-05-07 01:45:20 +0000

    fixed step 3

--- download_avatars.js
+++ download_avatars.js
@@ -4,9 +4,13 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-  });
+
 
 }
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+console.log("Errors:", err);
+console.log("Result:", result);
+});
