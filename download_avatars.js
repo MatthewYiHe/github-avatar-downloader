e06b0d6a330e5a2ff865efd9c1c47a37b8482ec6
MatthewYiHe <matthewhe1@hotmail.com>
2019-05-07 02:04:15 +0000

    step 5,6

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,19 @@
 var request = require('request');
+var token = require("./secrets.js");
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
 
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  request(options, function(err, res, body) {
     cb(err, body);
   });
-
 }
 
 
